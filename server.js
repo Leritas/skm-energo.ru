bb307632812e551eee99020158383591dacb3e47
leritas <din.fuso@mail.ru>
2022-05-23 01:54:54 +0300

    public folder setted up static files

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const express = require('express')
 const app = express()
 
 app.set('view engine', 'ejs')
+app.use(express.static(`${__dirname}/public`))
 
 app.get('/', (req,res) => {
     res.render("index", {
         title: 'СКМ - Энергосервис'
     })
 })
+
+app.get('/manufacturers', (req,res) => {
+    res.render('mfacrurers')
+})
+
 const productRouter = require('./routes/product')
 app.use('/product', productRouter)
 
