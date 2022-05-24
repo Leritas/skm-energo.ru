676f2bad6a2c35a120bd9ac57d74a9936a268595
leritas <din.fuso@mail.ru>
2022-05-24 22:10:32 +0300

    Desktop header done

--- server.js
+++ server.js
@@ -6,15 +6,23 @@ app.use(express.static(`${__dirname}/public`))
 
 app.get('/', (req,res) => {
     res.render("index", {
-        title: 'СКМ-Энергосервис'
+        title: 'СКМ-Энергосервис',
+        headLink: '<link rel="stylesheet" type="text/css" href="/styles/home.css">'
     })
 })
 
-app.get('/mfacturers', (req,res) => {
-    res.render('mfacturers', {
-        title: 'Производители | СКМ-Энергосервис'
+app.get('/casram', (req,res) => {
+    res.render('casram', {
+        title: 'Casram | Компоненты подвижного состава (Switzerland)| СКМ-Энергосервис'
     })
 })
+app.get('/lampar', (req,res) => {
+    res.render('lampar', {
+        title: 'Lampar | Каталог промышленных щёток (Italy) | СКМ-Энергосервис'
+    })
+})
+
+
 
 const productRouter = require('./routes/product')
 app.use('/product', productRouter)
