b1e650b558a9e08be8e7c5b5071241353ffa48d2
leritas <din.fuso@mail.ru>
2022-05-25 03:31:40 +0300

    Seems like all header adaptive

--- public/script.js
+++ public/script.js
@@ -1,5 +1,25 @@
 const logo = document.querySelector('.logo')
+const blackBg = document.querySelector('.black-bg-mobile')
+const burger = document.querySelector('.menu-burger')
+const menuClose = document.querySelector('.menu-close')
+const navMobile = document.querySelector('nav.mobile')
+const dropdownMobile = navMobile.querySelector('.dropdown')
 
 logo.addEventListener('click', () => {
     window.location.replace('/')
 })
+
+burger.addEventListener('click', toggleMobileMenu)
+blackBg.addEventListener('click', toggleMobileMenu)
+menuClose.addEventListener('click', toggleMobileMenu)
+
+function toggleMobileMenu() {
+    navMobile.classList.toggle('active')
+    blackBg.classList.toggle('active')
+}
+
+dropdownMobile.addEventListener('click', toggleDropdownMobile)
+
+function toggleDropdownMobile() {
+    dropdownMobile.classList.toggle('active')
+}
